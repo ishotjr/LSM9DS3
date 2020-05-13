c444e5e7016ccce6d2d489ee0ee63b277b3afb8f
ishotjr <github@ishotjr.com>
2020-05-13 00:47:52 -0400

    :satellite: :signal_strength: Add simple BLE

--- LSM9DS3.ino
+++ LSM9DS3.ino
@@ -1,4 +1,6 @@
 #include <Arduino_LSM9DS1.h>
+#include <ArduinoBLE.h>
+
 
 // calibration
 const int empty = 0;
@@ -21,7 +23,19 @@ int sensorValueTotal = 0;
 int sensorValueAverage = 0;
 
 
+BLEService cupService("1101");
+BLEUnsignedShortCharacteristic cupLevelChar("2101", BLERead | BLENotify);
+
 void setup() {
+  
+  Serial.begin(9600);
+  while (!Serial);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+    while (1);
+  }
 
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
@@ -35,58 +49,88 @@ void setup() {
     while (1);
   }
   
+  BLE.setLocalName("SmartCup");
+  BLE.setAdvertisedService(cupService);
+  cupService.addCharacteristic(cupLevelChar);
+  BLE.addService(cupService);
+  
+  BLE.advertise();
+  Serial.println("Bluetooth device active, waiting for connections...");
+  
 }
 
 void loop() {
   
   float x, y, z;
-
-  if (IMU.accelerationAvailable()) {
-
-    IMU.readAcceleration(x, y, z);
-
-    if (x <= (-1 + standingTolerance) && x >= (-1 - standingTolerance)) {
-      Serial.print("standing: ");
-        
-      // remove oldest value from total
-      sensorValueTotal -= sensorValues[sampleIndex];
-      
-      sensorValues[sampleIndex] = analogRead(sensorPin);
-      sensorValueTotal += sensorValues[sampleIndex];
-      sensorValueAverage = sensorValueTotal / samples;
-      
-      if (sensorValueAverage > filled) {
-        Serial.print("100% ");
-        setLed(255, 0, 0);
-      } else if (sensorValueAverage > threeQuarterFilled) {
-        Serial.print("75% ");
-        setLed(255, 127, 0);
-      } else if (sensorValueAverage > halfFilled) {
-        Serial.print("50% ");
-        setLed(255, 255, 0);
-      } else if (sensorValueAverage > quarterFilled) {
-        Serial.print("25% ");
-        setLed(0, 255, 0);
-      } else {
-        Serial.print("0% ");
-        setLed(0, 0, 255);
-      }
-
-      // wrap index if needed
-      sampleIndex++;
-      if (sampleIndex == samples)
-      {
-        sampleIndex = 0;
-      }
-  
-      Serial.println(sensorValueAverage);
-    } else {
-      Serial.println("not standing");
-        setLed(255, 0, 255);
-    }
     
-    delay(100);
+  BLEDevice central = BLE.central();
+
+  if (central) {
+    
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
+    digitalWrite(LED_BUILTIN, HIGH);
+    
+    while (central.connected()) {
+    
+      if (IMU.accelerationAvailable()) {
+    
+        IMU.readAcceleration(x, y, z);
+    
+        if (x <= (-1 + standingTolerance) && x >= (-1 - standingTolerance)) {
+          Serial.print("standing: ");
+            
+          // remove oldest value from total
+          sensorValueTotal -= sensorValues[sampleIndex];
+          
+          sensorValues[sampleIndex] = analogRead(sensorPin);
+          sensorValueTotal += sensorValues[sampleIndex];
+          sensorValueAverage = sensorValueTotal / samples;
+          
+          if (sensorValueAverage > filled) {
+            Serial.print("100% ");
+            cupLevelChar.writeValue(100);
+            setLed(255, 0, 0);
+          } else if (sensorValueAverage > threeQuarterFilled) {
+            Serial.print("75% ");
+            cupLevelChar.writeValue(75);
+            setLed(255, 127, 0);
+          } else if (sensorValueAverage > halfFilled) {
+            Serial.print("50% ");
+            cupLevelChar.writeValue(50);
+            setLed(255, 255, 0);
+          } else if (sensorValueAverage > quarterFilled) {
+            Serial.print("25% ");
+            cupLevelChar.writeValue(25);
+            setLed(0, 255, 0);
+          } else {
+            Serial.print("0% ");
+            cupLevelChar.writeValue(0);
+            setLed(0, 0, 255);
+          }
+    
+          // wrap index if needed
+          sampleIndex++;
+          if (sampleIndex == samples)
+          {
+            sampleIndex = 0;
+          }
+      
+          Serial.println(sensorValueAverage);
+        } else {
+          Serial.println("not standing");
+            setLed(255, 0, 255);
+        }
+        
+        delay(100);
+      }
+      
+    }
   }
+  
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.print("Disconnected from central: ");
+  Serial.println(central.address());
   
 }
 
